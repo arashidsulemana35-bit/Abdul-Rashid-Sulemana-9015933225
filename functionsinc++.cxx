c9bb7d89c89dac9e3f7792236caac9abd596c5c6
arashidsulemana35-bit <arashidsulemana35@gmail.com>
2026-03-02 10:47:41 +0000

    Add files via upload

---
+++ functionsinc++.cxx
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void happyBirthday(string name, int age);
+
+int main()
+{
+   string name="Bro";
+   int age=21;
+   
+  happyBirthday(name, age);
+ 
+	return 0;	
+	}
+	void happyBirthday(string name, int age){
+	
+	cout<<"Happy Birthday to "<<name<<'\n';
+	cout<<"Happy Birthday to "<<name<<'\n';
+	cout<<"Happy Birthday dear "<<name<<'\n';
+	cout<<"Happy Birthday to "<<name<<'\n';
+	
+	cout<<"You are now "<<age<<" years old\n";
+	
+}
