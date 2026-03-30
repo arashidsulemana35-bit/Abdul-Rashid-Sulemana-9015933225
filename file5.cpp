4744f53ee9af3cba8f68f5a251bbbc6fff2b732d
arashidsulemana35-bit <arashidsulemana35@gmail.com>
2026-03-30 13:56:01 +0330

    Add files via upload

---
+++ file5.cpp
@@ -0,0 +1,16 @@
+#include  <iostream>
+
+int main()
+{
+    //parenthesis
+    //multiplication & division
+    //addition & subtraction
+
+
+    int students = 6 - 5  + 4 * 3 / 2;
+
+
+    std::cout << students;
+
+    return 0;
+}
---
+++ file6.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main()
+{
+    //arithmetic operators = return the result of a specific
+    //                       arithmetic operation (+ - * /)
+
+    int students = 20;
+
+    //students = students + 1;
+    // students+=1; 
+    //students++;
+
+
+    //students = students - 2;
+    //students-=2;
+    //students--;
+
+    //students = students * 2;
+    //students*=2;
+
+    //students = students / 3;
+    //students/=3;
+
+    int remainder = students % 3;
+
+    std::cout << remainder;
+
+    return  0;
+}
